c6edb38c33fcdc42e34a02f0a022752a6a2e6c14
dalazaro <daryl.lazaro@gmail.com>
2017-11-21 16:33:28 -0800

    configured seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Cashier")
+User.create(name: "Baker")
+User.create(name: "Manager")
+
+Order.create(name: "Daryl", phone: "5105551234", size: "Medium", toppings: "Pepperoni", ready: false)
+Order.create(name: "James S.", phone: "2095557890", size: "Large", toppings: "Cheese", ready: false)
+Order.create(name: "Chris F.", phone: "8085554321", size: "Small", toppings: "Hawaiian", ready: false)
