7f2ce0026783721dd9cb7fb196b5afa3d61b6447
dalazaro <daryl.lazaro@gmail.com>
2017-11-21 16:33:42 -0800

    configured controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,45 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all.order('pickup ASC')
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    order = Order.new(order_params)
+    order.ready = false
+
+    if order.save
+      flash[:notice] = 'Order saved.'
+      redirect_to new_order_path
+    else
+      flash[:error] = 'There was an error in saving the order. Please try again.'
+      redirect_to new_order_path
+    end
+  end
+
+  def edit
+    @order = Order.find_by_id(params[:id])
+  end
+
+  def update
+    order = Order.find_by_id(params[:id])
+    order.update_attributes(order_params)
+    redirect_to orders_path(order)
+  end
+
+  def destroy
+    order = Order.find_by_id(params[:id])
+    order.destroy
+    redirect_to orders_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :phone, :pickup, :size, :toppings, :ready)
+  end
 end
